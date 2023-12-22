88a9d2696d27b17e69f1cf885362f7b3b0914d32
halfwit <michaelmisch1985@gmail.com>
2023-12-21 20:24:14 -0800

    add start of binpacking

--- 9hkd.c
+++ 9hkd.c
@@ -1,55 +1,48 @@
 #include <u.h>
 #include <libc.h>
 #include <draw.h>
+#include <bio.h>
+#include <ndb.h>
 #include <keyboard.h>
 #include <ctype.h>
 
 typedef struct W W;
 
 enum {
-	Mmod4 = 1<<0,
-	Mctl = 1<<1,
+    Mmod4 = 1<<0,
+    Mctl = 1<<1,
 	Mshift = 1<<2,
 
 	Step = 16,
 	Stepbig = 64,
+	Threshold = 64,
 
-	Fvisible = 1<<0,
-	Fcurrent = 1<<1,
-	Fsticky = 1<<2,
-	Ffullscreen = 1<<3,
-	ntags = 6,
+	Fcurrent = 1<<0,
+	Ffullscreen = 1<<1,
+	Fresizable = 1<<2,
+	Fvisible = 1<<3,
+
 };
 
 struct W {
-	int id;
-	Rectangle r;
-	int vd;
-	int tg;
-	int flags;
-	int stickyforced;
+    int id;
+    Rectangle r;
+    Rectangle cap;
+    int tg;
+    int flags;
+	char label[32];
 };
 
-static int vd = 1; /* current virtual desktop */
-static int wsys; /* rios /dev/wsys fd */
-static int mod;
-static int usetags;
+static Point *pts;
+static int npoints;
+/*static int gap;*/
 static W *ws, *wcur;
+static Ndb *db;
 static int wsn;
-static int tgvis[ntags] = {-1};
-static int vd2wcur[10] = {-1};
-static int tg2wcur[ntags] = {-1};
-
-static char *sticky[32] = {
-	"bar",
-	"cat clock",
-	"clock",
-	"faces",
-	"kbmap",
-	"stats",
-	"winwatch",
-	nil,
-};
+static int wsys; /* /dev/wsys fd */
+static int mod; /* modmask mode currently */
+static void wsupdate(void);
+static void binpack(void);
 
 static int
 wwctl(int id, int mode)
@@ -59,287 +52,6 @@ wwctl(int id, int mode)
 	snprint(s, sizeof(s), "/dev/wsys/%d/wctl", id);
 
 	return open(s, mode);
-}
-
-static void
-wsupdate(void)
-{
-	int i, k, n, f, seen, tn, dsn;
-	char s[256], *t[8];
-	W *newws, *w;
-	Dir *ds, *d;
-
-	seek(wsys, 0, 0);
-	if((dsn = dirreadall(wsys, &ds)) < 0)
-		sysfatal("/dev/wsys: %r");
-
-	newws = malloc(sizeof(W)*dsn);
-	wcur = nil;
-	for(i = 0, d = ds, w = newws; i < dsn; i++, d++){
-		if((f = wwctl(atoi(d->name), OREAD)) < 0)
-			continue;
-		n = read(f, s, sizeof(s)-1);
-		close(f);
-		if(n < 12)
-			continue;
-		s[n] = 0;
-		if((tn = tokenize(s, t, nelem(t))) < 6)
-			continue;
-
-		w->id = atoi(d->name);
-		w->r.min.x = atoi(t[0]);
-		w->r.min.y = atoi(t[1]);
-		w->r.max.x = atoi(t[2]);
-		w->r.max.y = atoi(t[3]);
-		w->vd = -1;
-		w->tg = -1;
-		w->flags = 0;
-		w->stickyforced = 0;
-
-		/* move over the current state of the window */
-		for(k = 0, seen = 0; k < wsn; k++){
-			if(ws[k].id == w->id){
-				w->vd = ws[k].vd;
-				w->tg = ws[k].tg;
-				w->flags = ws[k].flags & ~(Fvisible|Fcurrent);
-				w->stickyforced = ws[k].stickyforced;
-				if(w->flags & Ffullscreen)
-					w->r = ws[k].r;
-				seen = 1;
-				break;
-			}
-		}
-
-		/* update current state */
-		for(k = 4; k < tn; k++){
-			if(strcmp(t[k], "current") == 0){
-				w->flags |= Fcurrent;
-				wcur = w;
-				w->vd = vd;
-			}else if(strcmp(t[k], "visible") == 0){
-				w->flags |= Fvisible;
-				w->vd = vd;
-			}
-		}
-
-		if(!seen){
-			/* not seen previously - set the new state for it */
-			w->vd = vd;
-		}
-
-		/* because a different program can run in any window we have to re-read */
-		w->flags &= ~Fsticky;
-		if(w->stickyforced){
-			w->flags |= Fsticky;
-		}else{
-			snprint(s, sizeof(s), "/dev/wsys/%d/label", w->id);
-			if((f = open(s, OREAD)) >= 0){
-				n = read(f, s, sizeof(s)-1);
-				close(f);
-				if(n > 0){
-					s[n] = 0;
-					for(k = 0; k < nelem(sticky) && sticky[k] != nil; k++){
-						if(strcmp(sticky[k], s) == 0){
-							w->flags |= Fsticky;
-							break;
-						}
-					}
-				}
-			}
-		}
-		w++;
-	}
-
-	free(ds);
-	free(ws);
-	ws = newws;
-	wsn = w - newws;
-}
-
-static void
-spawn(char *s)
-{
-	if(rfork(RFPROC|RFNOWAIT|RFNAMEG|RFENVG|RFCFDG|RFREND) == 0)
-		execl("/bin/rc", "rc", s, nil);
-}
-
-static void
-closewindow(void)
-{
-	int f;
-
-	wsupdate();
-	if(wcur == nil || (f = wwctl(wcur->id, OWRITE)) < 0)
-		return;
-	fprint(f, "delete");
-	close(f);	
-}
-
-static void
-togglefullscreen(void)
-{
-	int f;
-
-	wsupdate();
-	if(wcur == nil || (f = wwctl(wcur->id, OWRITE)) < 0)
-		return;
-	wcur->flags ^= Ffullscreen;
-	if(wcur->flags & Ffullscreen)
-		fprint(f, "resize -r 0 0 9999 9999");
-	else
-		fprint(f, "resize -r %d %d %d %d", wcur->r.min.x, wcur->r.min.y, wcur->r.max.x, wcur->r.max.y);
-	close(f);
-}
-
-static void
-togglesticky(void)
-{
-	wsupdate();
-	if(wcur != nil)
-		wcur->stickyforced ^= 1;
-}
-
-static void
-tagaction(int nvd)
-{
-	int f, wcurf, flip;;
-	W *w;
-
-	wsupdate();
-	flip = 0;
-	if(mod == Mmod4){
-		wcurf = -1;
-		wcur = nil;
-		for(w = ws; w < ws+wsn; w++){
-			if(w->tg == nvd && (w->flags & Fsticky) == 0){
-				if(tgvis[nvd] > 0){
-					flip = -1;
-					if((f = wwctl(w->id, OWRITE)) >= 0){
-						fprint(f, "hide");
-						close(f);
-					}
-				} else {
-					flip = 1;	
-					if((f = wwctl(w->id, OWRITE)) >= 0){
-						fprint(f, "unhide");
-						if(tg2wcur[nvd] == w->id && wcurf < 0){
-							wcur = w;
-							wcurf = f;
-						}else
-							close(f);
-					}
-				}
-			}
-		}
-		if(wcur != nil){
-			fprint(wcurf, "top");
-			fprint(wcurf, "current");
-			close(wcurf);
-		}
-		tgvis[nvd] = flip;
-	} else if(mod == (Mmod4 | Mshift) && wcur != nil){
-		tgvis[nvd] = 1;
-		wcur->tg = nvd;
-		tg2wcur[nvd] = wcur->id;
-		wcur = nil;
-	}
-	int i;
-	fprint(3, "-- ");
-	for(i = 1; i <= ntags; i++) {
-		switch(tgvis[i]){
-		case -1:
-			fprint(3, "u ");
-			break;
-		case 0:
-			fprint(3, "o ");
-			break;
-		case 1:
-			fprint(3, "n ");
-			break;
-		}
-	}
-	fprint(3, "--\n");	
-}
-
-static void
-vdaction(int nvd)
-{
-	int f, wcurf;
-	W *w;
-
-	if(vd == nvd)
-		return;
-
-	wsupdate();
-	if(mod == Mmod4){
-		wcur = nil;
-		wcurf = -1;
-		vd2wcur[vd] = -1;
-		for(w = ws; w < ws+wsn; w++){
-			if(w->flags & Fvisible)
-				w->vd = vd;
-			else if(w->vd == vd)
-				w->vd = -1;
-
-			if(w->flags & Fcurrent)
-				vd2wcur[vd] = w->id;
-
-			if(w->vd == nvd && (w->flags & Fsticky) == 0){
-				if((f = wwctl(w->id, OWRITE)) >= 0){
-					fprint(f, "unhide");
-					if(vd2wcur[nvd] == w->id && wcurf < 0){
-						wcur = w;
-						wcurf = f;
-					}else
-						close(f);
-				}
-			}
-		}
-
-		if(wcur != nil){
-			fprint(wcurf, "top");
-			fprint(wcurf, "current");
-			close(wcurf);
-		}
-
-		for(w = ws; w < ws+wsn; w++){
-			if(w->vd != nvd && (w->flags & (Fsticky|Fvisible)) == Fvisible){
-				if((f = wwctl(w->id, OWRITE)) >= 0){
-					fprint(f, "hide");
-					close(f);
-				}
-			}
-		}
-
-		vd = nvd;
-		fprint(3, "%d\n", vd);
-	}else if(mod == (Mmod4 | Mshift) && wcur != nil && wcur->vd != nvd){
-		if((f = wwctl(wcur->id, OWRITE)) >= 0){
-			fprint(f, "hide");
-			wcur->vd = nvd;
-			vd2wcur[nvd] = wcur->id; /* bring to the top */
-			wcur = nil;
-			close(f);
-		}
-	}
-}
-
-static void
-arrowaction(int x, int y)
-{
-	int f;
-
-	wsupdate();
-	if(wcur == nil || (f = wwctl(wcur->id, OWRITE)) < 0)
-		return;
-
-	x *= (mod & Mctl) ? Stepbig : Step;
-	y *= (mod & Mctl) ? Stepbig : Step;
-	if((mod & Mshift) == 0)
-		fprint(f, "move -minx %+d -miny %+d", x, y);
-	else
-		fprint(f, "resize -maxx %+d -maxy %+d -minx %+d -miny %+d", x, y, -x, -y);
-	close(f);
 }
 
 static struct {
@@ -372,8 +84,6 @@ cycleaction(int x, int y)
 			wcur = w;
 			continue;
 		}
-		if((w->flags & Fsticky) != 0 || w->vd != vd)
-			continue;
 		if(w₀ == nil)
 			w₀ = w;
 		if(wcur != nil)
@@ -389,82 +99,254 @@ cycleaction(int x, int y)
 	wcur = w;
 }
 
+static void
+wsupdate(void)
+{
+	int i, k, n, f, tn, dsn;
+	char s[256], *t[8];
+	Ndbtuple *nt;
+	Ndbs ndbs;
+	W *newws, *w;
+	Dir *ds, *d;
+
+	seek(wsys, 0, 0);
+	if((dsn = dirreadall(wsys, &ds)) < 0)
+		sysfatal("/dev/wsys: %r");
+
+	newws = malloc(sizeof(W)*dsn);
+	wcur = nil;
+	for(i = 0, d = ds, w = newws; i < dsn; i++, d++){
+		if((f = wwctl(atoi(d->name), OREAD)) < 0)
+			continue;
+		n = read(f, s, sizeof(s)-1);
+		close(f);
+		if(n < 12)
+			continue;
+		s[n] = 0;
+		if((tn = tokenize(s, t, nelem(t))) < 6)
+			continue;
+
+		w->id = atoi(d->name);
+		w->r.min.x = atoi(t[0]);
+		w->r.min.y = atoi(t[1]);
+		w->r.max.x = atoi(t[2]);
+		w->r.max.y = atoi(t[3]);
+		w->tg = -1;
+		w->flags = 0;
+
+		/* Get old values */
+		for(k = 0; k < wsn; k++){
+			if(ws[k].id == w->id){
+				w->tg = ws[k].tg;
+				snprint(w->label, sizeof(ws[k].label), ws[k].label);
+				w->flags = ws[k].flags & ~(Fvisible|Fcurrent);
+				if(w->flags &  Ffullscreen)
+					w->r = ws[k].r;
+				break;
+			}
+		}
+		for(k = 4; k < tn; k++){
+			if(strcmp(t[k], "current") == 0){
+				w->flags |= Fcurrent;
+				w->flags |= Fvisible;
+				wcur = w;
+				break;
+			} else if(strcmp(t[k], "visible") == 0){
+				w->flags |= Fvisible;
+			}
+		}
+
+		snprint(s, sizeof(s), "/dev/wsys/%d/label", w->id);
+		if((f = open(s, OREAD)) >= 0){
+			n = read(f, s, sizeof(s)-1);
+			close(f);
+			if(n > 0){
+				s[n] = 0;
+				/* Label is different, parse */
+				if(strcmp(w->label, s) != 0){
+					w->cap.min.x = 0;
+					w->cap.min.y = 0;
+					w->cap.max.x = 0;
+					w->cap.max.y = 0;
+					w->tg = -1;
+					/* Parse min/max, resizable flag, and tag group for label */
+					nt = ndbsearch(db, &ndbs, "label", s);
+					for(; nt != nil; nt = nt->entry){
+						if(strcmp(nt->attr, "maxx") == 0)
+							w->cap.max.x = atoi(nt->val);
+						if(strcmp(nt->attr, "maxy") == 0)
+							w->cap.max.y = atoi(nt->val);
+						if(strcmp(nt->attr, "minx") == 0)
+							w->cap.min.x = atoi(nt->val);
+						if(strcmp(nt->attr, "miny") == 0)
+							w->cap.min.y = atoi(nt->val);
+						if(strcmp(nt->attr, "tag") == 0)
+							w->tg = atoi(nt->val);
+					}
+					ndbfree(nt);
+					if(w->cap.min.x == 0 && w->cap.min.y == 0 && w->cap.max.x == 0 && w->cap.max.y == 0){
+						w->flags |= Fresizable;
+						w->cap.min.y = screen->r.min.y / 2;
+						w->cap.max.y = screen->r.max.y;
+						w->cap.min.x = w->r.min.y;
+						w->cap.max.x = w->r.max.x;
+					}
+				}
+			}
+			snprint(w->label, sizeof s, s);
+		}
+		w++;
+	}
+	
+	free(ds);
+	free(ws);
+	ws = newws;
+	wsn = w - newws;
+}
+
+static void
+binpack(void)
+{
+	Rectangle r, rt;
+    /**
+     * [x] Sort wins
+	 * [ ] set bounds.min to win1
+	 * [ ] start walking down point array
+	 * [ ] check if arena can accommodate, check virt then check horizontal
+	 * [ ] adjust rt.min.x + rt.min.y as needed and attempt pack
+	 * [ ] set r = rt if we fit
+	 * [ ] update points, if Pn+2 is up and to left by under Threshold in each direction, remove points
+     */
+	qsort(ws, wsn, sizeof(*ws), cyclecmp);
+	r = screen->r;
+	r.min.y = ws->cap.min.y;
+	r.min.x = ws->cap.min.x;
+	
+}
+
+static void
+spawn(char *s)
+{
+	if(rfork(RFPROC|RFNOWAIT|RFNAMEG|RFENVG|RFCFDG|RFREND) == 0)
+		execl("/bin/rc", "rc", s, nil);
+}
+
+
+static void
+togglefullscreen(void)
+{
+	int f;
+
+	wsupdate();
+	if(wcur == nil || (f = wwctl(wcur->id, OWRITE)) < 0)
+		return;
+	wcur->flags ^= Ffullscreen;
+	if(wcur->flags & Ffullscreen)
+		fprint(f, "resize -r 0 0 9999 9999");
+	else
+		fprint(f, "resize -r %d %d %d %d", wcur->r.min.x, wcur->r.min.y, wcur->r.max.x, wcur->r.max.y);
+	close(f);
+}
+
+static void
+widthaction(int x)
+{
+	int f;
+
+	wsupdate();
+	if(wcur == nil || (f = wwctl(wcur->id, OWRITE)) < 0)
+		return;
+
+	if((wcur->flags & Fresizable) == 0)
+		return;
+
+	x *= (mod & Mctl) ? Stepbig : Step;
+	fprint(f, "resize -maxx %+d -minx %+d", x, -x);
+	close(f);
+	binpack();
+}
+
+static void
+tagaction(int nvd)
+{
+	int f, wcurf;
+	W *w;
+	wsupdate();
+	if((mod & Mshift) != 0){
+		wcur->tg = nvd;
+	} else {
+		for(w = ws; w < ws+wsn; w++){
+			if(w->tg == nvd){
+				if((f = wwctl(w->id, OWRITE)) >= 0){
+					fprint(f, w->flags & Fvisible ? "hide" : "unhide");
+					w->flags ^= Fvisible;
+					close(f);
+				}
+				/* Make sure we keep a visible window */
+				if(wcur == nil || w->id == wcur->id && (wcur->flags & Fvisible) == 0)
+					cycleaction(1, 0);
+			}
+		}
+		binpack();
+	}
+}
+
+static void
+closewindow(void)
+{
+	int f;
+
+	wsupdate();
+	if(wcur == nil || (f = wwctl(wcur->id, OWRITE)) < 0)
+		return;
+	fprint(f, "delete");
+	close(f);
+	binpack();
+	/* Try to focus the next visible window */
+	cycleaction(1, 0);
+}
+
 static int
 keyevent(char c, Rune r)
 {
-	if(c == 'c'){
-		if(r == 'n' && mod == Mmod4){
-			spawn("fmenu");
-			return 0;
-		}
-		if(r == 'd' && mod == Mmod4){
-			spawn("dmenu");
-			return 0;
-		}
-		if(r == 't' && mod == Mmod4){
-			spawn("window");
-			return 0;
-		}
-		if(r == 'c' && mod == Mmod4){
-			closewindow();
-			return 0;
-		}
-		if(r == 'f' && mod == Mmod4){
-			togglefullscreen();
-			return 0;
-		}
-		if(r == 's' && mod == Mmod4){
-			togglesticky();
-			return 0;
-		}
-		if(r == Kup){
-			arrowaction(0, -1);
-			return 0;
-		}
-		if(r == Kdown){
-			arrowaction(0, 1);
-			return 0;
-		}
-		if(r == Kleft){
-			arrowaction(-1, 0);
-			return 0;
-		}
-		if(r == Kright){
-			arrowaction(1, 0);
-			return 0;
-		}
-		if(r == 'h' && mod == Mmod4){
-			cycleaction(-1, 0);
-			return 0;
-		}
-		if(r == 'l' && mod == Mmod4){
-			cycleaction(1, 0);
-			return 0;
-		}
-		if(r == 'j' && mod == Mmod4){
-			cycleaction(0, 1);
-			return 0;
-		}
-		if(r == 'k' && mod == Mmod4){
-			cycleaction(0, -1);
-			return 0;
-		}
-		if(r >= '0' && r <= '9' && (mod & Mctl) == 0){
-			if(usetags)
+	if(c == 'c' && (mod & Mmod4) != 0){
+		/* TODO: This becomes a lookup */
+		switch(r) {
+		case 'n': spawn("fmenu"); break;
+		case 'd': spawn("dmenu"); break;
+		case 't': spawn("window"); break;
+		case 'c': closewindow(); break;
+		case 'f': togglefullscreen(); break;
+		case 'h': cycleaction(-1, 0); break;
+		case 'l': cycleaction(1, 0); break;
+		case 'j': cycleaction(0, 1); break;
+		case 'k': cycleaction(0, -1); break;
+		default:
+			if (r >= '0' && r <= '9'){
 				tagaction(r - '0');
-			else
-				vdaction(r - '0');
-			return 0;
+				return 0;
+			}
+			return -1;
 		}
-	}
-	/* mod4 + shift + 1…0 yields a shifted value on 'c': workaround */
-	if(c == 'k' && mod == (Mmod4|Mshift) && r >= '0' && r <= '9'){
-		if(usetags)
-			tagaction(r - '0');
-		else
-			vdaction(r - '0');
 		return 0;
 	}
+	if(c == 'k' && (mod & Mshift) == 0)
+		return 0;
+	if(c == 'k' && mod == (Mmod4|Mshift)) {
+		if (r == 'l' || r == 'L'){
+			widthaction(1);
+			return 0;
+		}
+		if (r == 'h' || r == 'H'){
+			widthaction(-1);
+			return 0;
+		}
+		if (r >= '0' && r <= '9'){
+			tagaction(r - '0');
+			return 0;
+		}
+	}
+
 	/* don't bother to properly deal with handling shifted digit keys */
 	if((mod & Mshift) != 0 && (c == 'c' || c == 'k' || c == 'K'))
 		return ispunct(r) ? 0 : -1;
@@ -472,7 +354,7 @@ keyevent(char c, Rune r)
 	return -1;
 }
 
-static void
+static int
 process(char *s)
 {
 	char b[128], *p;
@@ -497,7 +379,6 @@ process(char *s)
 			n = strlen(p);
 			memmove(b+o, p, n);
 			o += n;
-			p += n;
 			break;
 		}
 
@@ -507,66 +388,49 @@ process(char *s)
 		}
 	}
 
-	/* all runes filtered out - ignore completely */
-	if(o == 1 && p-s > 1)
-		return;
-
 	b[o++] = 0;
-	if(write(1, b, o) != o)
-		exits(nil);
+	return (o > 1 && write(1, b, o) <= 0) ? -1 : 0;
 }
 
 static void
 usage(void)
 {
-	fprint(2, "usage: %s [-t] [-s label]\n", argv0);
+	fprint(2, "usage: %s [-d wdb]\n", argv0);
 	exits("usage");
 }
 
 void
 main(int argc, char **argv)
 {
-	char b[128];
+	char b[128], s[64];
 	int i, j, n;
 
-	for(n = 0; sticky[n] != nil; n++)
-		;
-
 	ARGBEGIN{
-	case 's':
-		if(n >= nelem(sticky))
-			sysfatal("ewwww");
-		sticky[n++] = EARGF(usage());
+	case 'd':
+		s = EARGF(usage());
 		break;
-	case 't':
-		usetags++;
-		break;
-	default:
-		usage();
 	}ARGEND
 
 	if((wsys = open("/dev/wsys", OREAD)) < 0)
 		sysfatal("%r");
 
-	/* initial state */
+	if(s == nil)
+		snprint(s, sizeof(s), "%s/lib/wdb", getenv("home"));
+	db = ndbopen(s);
+	if(db == nil)
+		sysfatal("no db found");
+
+	/* Set our initial state */
 	wsupdate();
-	for(i = 0; i < wsn; i++)
-		ws[i].vd = vd;
-	fprint(3, "%d\n", vd);
-
+	//printtags();
+	
 	for(i = 0;;){
-		if((n = read(0, b+i, sizeof(b)-i)) <= 0)
+		if((n = read(0, b, sizeof(b)-1)) <= 0)
 			break;
-		n += i;
-		for(j = 0; j < n; j++){
-			if(b[j] == 0){
-				process(b+i);
-				i = j+1;
-			}
-		}
-		memmove(b, b+i, j-i);
-		i -= j;
+		b[n] = 0;
+		if(process(b) != 0)
+			break;
 	}
-
 	exits(nil);
 }
+
